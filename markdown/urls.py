c2b9858a855efbf3ca9c714eca56fd4838bdb9e4
samparly <samparly@gmail.com>
2016-10-26 21:06:54 -0400

    test

--- markdown/urls.py
+++ markdown/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from markdown.views import index
+from markdown.views import index, document
 
 
 urlpatterns = [
-  url(r'^', index)
+  url(r'^', index),
+  url(r'doc', document),
 ]
