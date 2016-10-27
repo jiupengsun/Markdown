8a94890c382ad30262c421feee264af0af4c2a8b
samparly <samparly@gmail.com>
2016-10-26 21:05:28 -0400

    test

--- markdown/views.py
+++ markdown/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,3 +6,12 @@ from django.shortcuts import render
 def index(request):
   if request.method == "GET":
     return render(request, 'index.html')
+
+def document(request):
+  if request.method == "GET":
+    context = {
+      'flag': 1,
+      'message' : 'success',
+      'data': {}
+    }
+    return JsonResponse(context)
